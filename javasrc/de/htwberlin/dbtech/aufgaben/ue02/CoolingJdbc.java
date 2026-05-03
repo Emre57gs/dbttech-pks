c25754a1861d46883f7afe1d6b75771575a7f23c
Emre57gs <Emre.Bas@Student.HTW-Berlin.de>
2026-05-03 20:24:51 +0200

    Methode 2 fertig

--- javasrc/de/htwberlin/dbtech/aufgaben/ue02/CoolingJdbc.java
+++ javasrc/de/htwberlin/dbtech/aufgaben/ue02/CoolingJdbc.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+import de.htwberlin.dbtech.exceptions.CoolingSystemException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -36,7 +37,7 @@ public class CoolingJdbc implements ICoolingJdbc {
         L.info("getSampleKinds: start");
         // TODO Auto-generated method stub
         List<String> sampleKind = null;
-        String sql = "select * from  samplekind order by text asc"; // SQL, kein Semikohlon
+        String sql = "select * from  samplekind order by text asc"; // SQL, kein Semikolon
         PreparedStatement p = null; // Connection
         ResultSet rs = null;
 
@@ -60,7 +61,29 @@ public class CoolingJdbc implements ICoolingJdbc {
     public Sample findSampleById(Integer sampleId) {
         L.info("findSampleById: sampleId: " + sampleId);
         // TODO Auto-generated method stub
-        return null;
+        String sql = "SELECT * FROM Sample WHERE SampleID = ?"; // ? Platzhalter für p
+        PreparedStatement p = null;
+        ResultSet rs = null;
+
+        try {
+            p = useConnection().prepareStatement(sql);
+            p.setInt(1, sampleId);
+            rs = p.executeQuery();
+            // Test 1 baut Objekt
+            if (rs.next()) {
+                Sample s = new Sample();
+                s.setSampleId(rs.getInt("SampleID"));
+                s.setSampleKindId(rs.getInt("SampleKindID"));
+                s.setExpirationDate(rs.getDate("ExpirationDate").toLocalDate());
+                return s;
+                // Test 2 wirft Exception
+            } else {
+                throw new CoolingSystemException("Sample not found");
+            }
+
+        } catch (SQLException e) {
+            throw new CoolingSystemException(e);
+        }
     }
 
     @Override
