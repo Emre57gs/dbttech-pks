bba4d7d754980da714002f09120c5b4804b03146
Emre57gs <Emre.Bas@Student.HTW-Berlin.de>
2026-06-15 17:56:55 +0200

    PW change

--- javasrc/de/htwberlin/dbtech/utils/DbCred.java
+++ javasrc/de/htwberlin/dbtech/utils/DbCred.java
@@ -4,6 +4,6 @@ public interface DbCred {
     String driverClass = "oracle.jdbc.driver.OracleDriver";
     String url = "jdbc:oracle:thin:@icla3lxc.f4.htw-berlin.de:1521:free";
     String user = "u579548";
-    String password = "p579548";
+    String password = "EmreSinop";
     String schema = "u579548";
 }
